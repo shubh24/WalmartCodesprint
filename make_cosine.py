402058728123ef8a1a0157f2e1dcb74481310bcb
shubh24 <shubhankar24@gmail.com>
2016-11-05 23:22:14 +0530

    dictionary of similar items(cosine)

--- make_cosine.py
+++ make_cosine.py
@@ -26,12 +26,29 @@ test = open("test_ref.csv", "r")
 test_reader = csv.reader(test)
 
 test_arr = {}
-for i in test_arr:    
-    test_arr.update({i[0]:i[1:]})
+for i in test_reader:    
+    val = i[1:]
+    val_int = [int(j) for j in val]
+    test_arr.update({int(i[0]):val_int})
 
 train_arr = {}
 for i in train_reader:    
-    train_arr.update({i[0]:i[1:]})
+    val = i[1:]
+    val_int = [int(j) for j in val]
+    train_arr.update({int(i[0]):val_int})
 
 
 cos_dict = {i:{} for i in test_arr}
+c = 0
+for test_case in test_arr:
+    print c
+    c+=1
+    test_case_cos_arr = {}
+
+    for train_case in train_arr:
+        test_case_cos_arr.update({train_case:get_cosine(train_arr[train_case], test_arr[test_case])})
+    
+    cos_dict[test_case] = sorted(test_case_cos_arr, key = lambda x : test_case_cos_arr[x], reverse = True)[:50]
+
+with open('cos_dict.pickle', 'wb') as handle:
+    pickle.dump(cos_dict, handle)
